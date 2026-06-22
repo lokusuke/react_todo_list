35fc282851fbe2c99e8d78f18295c77fadb218d5
lokusuke <suzloku63@gmail.com>
2026-06-22 19:16:17 +0900

    Github Pages用にファイルを追加

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [tailwindcss(), react()],
-});
+  base: command === "build" ? "/react_todo_list/" : "/",
+}));
